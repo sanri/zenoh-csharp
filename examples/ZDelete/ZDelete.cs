81c3a51b93a8c66fab58d25eeb920e5c08a3dd25
sanri <engineer_tang@qq.com>
2025-03-22 19:24:48 +0800

    change examples property, LangVersion to 8

--- examples/ZDelete/ZDelete.cs
+++ examples/ZDelete/ZDelete.cs
@@ -2,60 +2,62 @@ using System;
 using CommandLine;
 using Zenoh;
 
-namespace ZDelete;
-
-public class Program
+namespace ZDelete
 {
-    static void Main(string[] args)
+    public class Program
     {
-        var arguments = Parser.Default.ParseArguments<Args>(args);
-        var isOk = true;
-        arguments.WithNotParsed(e => { isOk = false; });
-        if (!isOk) return;
-
-        var config = arguments.Value.ToConfig();
-        if (config is null) return;
-
-        Console.WriteLine("Opening session...");
-        var r = Session.Open(config, out var session);
-        if (session is null)
+        static void Main(string[] args)
         {
-            Console.WriteLine($"Opening session unsuccessful! result: {r}");
-            return;
+            var arguments = Parser.Default.ParseArguments<Args>(args);
+            var isOk = true;
+            arguments.WithNotParsed(e => { isOk = false; });
+            if (!isOk) return;
+
+            var config = arguments.Value.ToConfig();
+            if (config is null) return;
+
+            Console.WriteLine("Opening session...");
+            var r = Session.Open(config, out var session);
+            if (session is null)
+            {
+                Console.WriteLine($"Opening session unsuccessful! result: {r}");
+                return;
+            }
+
+            Console.WriteLine("Opening session successful!\n");
+
+            string keyexprStr = "demo/example/zenoh-cs-delete";
+            var keyexpr = Keyexpr.FromString(keyexprStr);
+            if (keyexpr is null) goto Exit;
+
+            var options = new DeleteOptions();
+
+            r = session.Delete(keyexpr, options);
+
+            var printStr = r != Result.Ok
+                ? $"Deleting data failed! result: {r}, key: {keyexprStr}"
+                : $"Deleting data succeeded! key: {keyexprStr}";
+            Console.WriteLine(printStr);
+
+            Exit:
+            session.Close();
+            Console.WriteLine("exit");
         }
-
-        Console.WriteLine("Opening session successful!\n");
-
-        string keyexprStr = "demo/example/zenoh-cs-delete";
-        var keyexpr = Keyexpr.FromString(keyexprStr);
-        if (keyexpr is null) goto Exit;
-
-        var options = new DeleteOptions();
-
-        r = session.Delete(keyexpr, options);
-
-        var printStr = r != Result.Ok
-            ? $"Deleting data failed! result: {r}, key: {keyexprStr}"
-            : $"Deleting data succeeded! key: {keyexprStr}";
-        Console.WriteLine(printStr);
-
-        Exit:
-        session.Close();
-        Console.WriteLine("exit");
     }
-}
 
-public class Args
-{
-    [Option('c', "config", Required = false, HelpText = "Zenoh config file.")]
-    public string? ConfigFilePath { get; set; } = null;
-
-    public Config? ToConfig()
+    public class Args
     {
-        var config = ConfigFilePath == null ? Config.Default() : Config.FromFile(ConfigFilePath);
+        [Option('c', "config", Required = false, HelpText = "Zenoh config file.")]
+        public string? ConfigFilePath { get; set; } = null;
 
-        if (config is not null) return config;
-        Console.WriteLine("load config file error!");
-        return null;
+        public Config? ToConfig()
+        {
+            var config = ConfigFilePath == null ? Config.Default() : Config.FromFile(ConfigFilePath);
+
+            if (config is null == false) return config;
+
+            Console.WriteLine("load config file error!");
+            return null;
+        }
     }
 }
--- examples/ZPub/ZPub.cs
+++ examples/ZPub/ZPub.cs
@@ -3,80 +3,82 @@ using System.Threading;
 using CommandLine;
 using Zenoh;
 
-namespace ZPub;
-
-public class Program
+namespace ZPub
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        var arguments = Parser.Default.ParseArguments<Args>(args);
-        var isOk = true;
-        arguments.WithNotParsed(e => { isOk = false; });
-        if (!isOk) return;
-
-        var config = arguments.Value.ToConfig();
-        if (config is null) return;
-
-        Console.WriteLine("Opening session...");
-        var r = Session.Open(config, out var session);
-        if (session is null)
+        public static void Main(string[] args)
         {
-            Console.WriteLine($"Opening session unsuccessful! result: {r}");
-            return;
-        }
+            var arguments = Parser.Default.ParseArguments<Args>(args);
+            var isOk = true;
+            arguments.WithNotParsed(e => { isOk = false; });
+            if (!isOk) return;
 
-        Console.WriteLine("Opening session successful!\n");
+            var config = arguments.Value.ToConfig();
+            if (config is null) return;
 
-        string keyStr = "demo/example/zenoh-cs-pub/string";
-        var keyexpr = Keyexpr.FromString(keyStr);
-        if (keyexpr is null) goto Exit;
-
-        var publisherOptions = new PublisherOptions();
-        publisherOptions.SetEncoding(new Encoding(EncodingId.TextPlain));
-
-        r = session.DeclarePublisher(keyexpr, publisherOptions, out Publisher? publisher);
-        if (publisher is null)
-        {
-            Console.WriteLine($"Declare publisher unsuccessful! result: {r}");
-            goto Exit;
-        }
-
-        Console.WriteLine($"Declaring publisher on {keyexpr}");
-
-        for (int i = 0; i < 100; i++)
-        {
-            Thread.Sleep(1000);
-            var payloadStr = $"[{i}] Pub from CS!";
-            var payload = ZBytes.FromString(payloadStr);
-            r = publisher.Put(payload, new PublisherPutOptions());
-            if (r != Result.Ok)
+            Console.WriteLine("Opening session...");
+            var r = Session.Open(config, out var session);
+            if (session is null)
             {
-                Console.WriteLine($"Publisher put unsuccessful! result: {r}");
+                Console.WriteLine($"Opening session unsuccessful! result: {r}");
+                return;
+            }
+
+            Console.WriteLine("Opening session successful!\n");
+
+            string keyStr = "demo/example/zenoh-cs-pub/string";
+            var keyexpr = Keyexpr.FromString(keyStr);
+            if (keyexpr is null) goto Exit;
+
+            var publisherOptions = new PublisherOptions();
+            publisherOptions.SetEncoding(new Encoding(EncodingId.TextPlain));
+
+            r = session.DeclarePublisher(keyexpr, publisherOptions, out Publisher? publisher);
+            if (publisher is null)
+            {
+                Console.WriteLine($"Declare publisher unsuccessful! result: {r}");
                 goto Exit;
             }
 
-            Console.WriteLine($"Publisher put data {payloadStr}");
+            Console.WriteLine($"Declaring publisher on {keyexpr}");
+
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(1000);
+                var payloadStr = $"[{i}] Pub from CS!";
+                var payload = ZBytes.FromString(payloadStr);
+                r = publisher.Put(payload, new PublisherPutOptions());
+                if (r != Result.Ok)
+                {
+                    Console.WriteLine($"Publisher put unsuccessful! result: {r}");
+                    goto Exit;
+                }
+
+                Console.WriteLine($"Publisher put data {payloadStr}");
+            }
+
+            publisher.Undeclare();
+
+            Exit:
+            session.Close();
+            Console.WriteLine("exit");
         }
-
-        publisher.Undeclare();
-
-        Exit:
-        session.Close();
-        Console.WriteLine("exit");
     }
-}
 
-public class Args
-{
-    [Option('c', "config", Required = false, HelpText = "Zenoh config file.")]
-    public string? ConfigFilePath { get; set; } = null;
-
-    public Config? ToConfig()
+    public class Args
     {
-        var config = ConfigFilePath == null ? Config.Default() : Config.FromFile(ConfigFilePath);
+        [Option('c', "config", Required = false, HelpText = "Zenoh config file.")]
+        public string? ConfigFilePath { get; set; } = null;
 
-        if (config is not null) return config;
-        Console.WriteLine("load config file error!");
-        return null;
+        public Config? ToConfig()
+        {
+            var config = ConfigFilePath == null ? Config.Default() : Config.FromFile(ConfigFilePath);
+
+            if (config is null == false) return config;
+
+            Console.WriteLine("load config file error!");
+            return null;
+        }
     }
 }
